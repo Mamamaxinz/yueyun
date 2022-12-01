64a268cb0d7e5e911b82b721687fe714a66ddcb2
Mamamaxinz <mazozo0311@icloud.com>
2022-12-01 13:57:02 +0800

    initial commit

---
+++ custom.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+	//fullpage plugin initialization
+	$('#fullpage').fullpage({
+		anchors:['firstPage', 'secondPage', 'thirdPage', '4thPage', '5thPage', '6thPage'],
+		menu:'#js-menu',
+		autoScrolling: false
+	});
+
+	// logo change when scroll
+	$(window).on('scroll',function(){
+		var scrollDistance = $(window).scrollTop();
+		var $header = $(".js-header");
+		if(scrollDistance > 80){
+			$header.addClass("header--scrolling");
+		}
+		else{
+			$header.removeClass("header--scrolling")
+		}
+	})
+
+	// carousel plugin initialization
+	$('.owl-carousel').owlCarousel({
+    	loop:true,
+    	margin:16,
+    	nav:true,
+    	navText:[],
+    	dots:false,
+    	responsive:{
+        	0:{
+            	items:1
+        	},
+        	768:{
+            	items:2
+        	},
+        	1000:{
+            	items:2
+        	}
+    	}
+	})
+
+});
